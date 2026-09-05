9a77bc41c00a580bc8e2bdb6c2c2559ef459c2ad
Paulo Nogueira Silva <paulonogueirasilva@hotmail.com>
2026-09-05 20:43:31 -0300

    20260905 Inclusão mensagem orientação final.

--- Mac/MacOsYouTube_Playlist_to_Mac.sh
+++ Mac/MacOsYouTube_Playlist_to_Mac.sh
@@ -53,3 +53,5 @@ yt-dlp -f 'ba[ext=m4a]' \
 --replace-in-metadata "playlist_uploader" " - Topic" "" \
 -o "%(album_artist)s - %(album)s/%(playlist_index)02d - %(title)s.%(ext)s" \
 "$URL_CORRIGIDA"
+
+echo -e "\nTransfira para o Ubuntu navidrome com MacOsMac_to_Ubuntu_navidrome(.sh)...\n$URL_CORRIGIDA\n"
